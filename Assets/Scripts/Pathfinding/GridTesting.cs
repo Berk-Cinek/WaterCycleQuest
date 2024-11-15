631334a9f3bc8cc71e495684278e83f861558a54
Berk-Cinek <cinek.berkutkan@gmail.com>
2024-11-16 00:23:39 +0300

    Grid Generic Upgrade

--- Assets/Scripts/Pathfinding/GridTesting.cs
+++ Assets/Scripts/Pathfinding/GridTesting.cs
@@ -5,12 +5,13 @@ using CodeMonkey.Utils;
 
 public class GridTesting : MonoBehaviour
 {
-    private Grid grid;
-    
+    private Grid<HeatMapGridObject> grid;
+
     //making a grid
     void Start()
     {
-        grid = new Grid(10, 3, 1f,new Vector3(0, 0));        
+        //after vector3.zero we have func method signiture that passes it all to our heatmap constructor
+        grid = new Grid<HeatMapGridObject>(20, 10, 1f, Vector3.zero, (Grid<HeatMapGridObject>g, int x, int y) => new HeatMapGridObject(g, x ,y));
     }
 
     private void Update()
@@ -18,12 +19,46 @@ public class GridTesting : MonoBehaviour
         //set given value
         if (Input.GetMouseButtonDown(0))
         {
-            grid.SetValue(UtilsClass.GetMouseWorldPosition(), 56);
+            Vector3 position = UtilsClass.GetMouseWorldPosition();
+            HeatMapGridObject heatMapGridObject = grid.GetGridObject(position);
+            if (heatMapGridObject != null) heatMapGridObject.AddValue(5);
         }
         // get value
         if (Input.GetMouseButtonDown(1))
         {
-            Debug.Log(grid.GetValue(UtilsClass.GetMouseWorldPosition()));
+            Debug.Log(grid.GetGridObject(UtilsClass.GetMouseWorldPosition()));
         }
     }
+
+    public class HeatMapGridObject {
+
+        private const int MIN = 0;
+        private const int MAX = 100;
+
+        private Grid<HeatMapGridObject> grid;
+        public int value;
+        private int x;
+        private int y;
+        
+        public HeatMapGridObject(Grid<HeatMapGridObject> grid, int x, int y)
+        {
+            this.grid = grid;
+            this.x = x;
+            this.y = y;
+        }
+
+        public void AddValue(int addValue){
+            value += addValue;
+            value = Mathf.Clamp(value, MIN, MAX);
+            grid.TriggerGameObjectChanged(x, y);
+        }
+        public float GetValueNormalized() { 
+            return (float)value /MAX;
+        }
+        public override string ToString(){
+            return value.ToString();
+        }
+
+
+        }
 }
--- Assets/Scripts/Pathfinding/Pathfinding.cs
+++ Assets/Scripts/Pathfinding/Pathfinding.cs
@@ -4,5 +4,6 @@ using UnityEngine;
 
 public class Pathfinding{
 
+    private Grid<PathNode> grid;
 
 }
