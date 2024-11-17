d47a978e78072abedc4cbbfc4d46d2cb211fa9f0
Yağız Haşit <yagizhasit@hotmail.com>
2024-11-17 17:04:44 +0300

    Shooting Targets

--- Assets/Scripts/Bullet/Bullet.cs
+++ Assets/Scripts/Bullet/Bullet.cs
@@ -1,4 +1,3 @@
-using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,48 +5,54 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     private Vector3 shootDir;
-    public void Setup(Vector3 shootDir)
+    private Vector3 startPosition;
+    private float moveSpeed;
+    private int damage;
+    private float range;
+
+    public void Setup(Vector3 shootDir, float moveSpeed = 100f, int damage = 10, float range = 10f)
     {
         this.shootDir = shootDir;
+        this.moveSpeed = moveSpeed;
+        this.damage = damage;
+        this.range = range;
+
+        startPosition = transform.position; 
         transform.eulerAngles = new Vector3(0, 0, GetAngleFromVectorFloat(shootDir));
-        Destroy(gameObject, 5f);
     }
 
     private void Update()
     {
-        float moveSpeed = 100f;
+        
         transform.position += shootDir * moveSpeed * Time.deltaTime;
 
         
-        float hitDetectionSize = 3f;
-        Target target = Target.GetClosest(transform.position, hitDetectionSize);
-        if (target != null)
+        if (Vector3.Distance(startPosition, transform.position) >= range)
         {
-            target.Damage();
             Destroy(gameObject);
         }
-        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        Target target = collider.GetComponent<Target>();
+        if (target != null)
+        {
+            target.Damage(damage); 
+            Destroy(gameObject); 
+        }
+        else
+        {
+            Debug.Log("Bullet hit something else: " + collider.name); 
+        }
     }
 
     public static float GetAngleFromVectorFloat(Vector3 dir)
     {
         dir = dir.normalized;
         float n = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-        if (n < 0) n += 360;
-
-        return n;
+        return n < 0 ? n + 360 : n;
     }
-
-    /*
-    private void OnTriggerEnter2D(Collider2D collider)
-    {
-        Target target = collider.GetComponent<Target>();
-        if (target != null)
-        {
-            target.Damage();
-            Destroy(gameObject);
-        }
-    */
-    }
+}
 
 
--- Assets/Scripts/Target/Target.cs
+++ Assets/Scripts/Target/Target.cs
@@ -1,72 +1,20 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Target : MonoBehaviour
 {
-    private static List<Target> targetList;
+    [SerializeField] private int health = 100; // Default health of the target
 
-    
-    public static Target GetClosest(Vector3 position, float maxRange)
+    public void Damage(int damageAmount)
     {
-        Target closest = null;
+        health -= damageAmount;
+        Debug.Log($"Target {gameObject.name} hit! Remaining health: {health}");
 
-        foreach (Target target in targetList)
+        if (health <= 0)
         {
-            float distanceToTarget = Vector3.Distance(position, target.GetPosition());
-            if (distanceToTarget <= maxRange)
-            {
-                if (closest == null || distanceToTarget < Vector3.Distance(position, closest.GetPosition()))
-                {
-                    closest = target;
-                }
-            }
+            Destroy(gameObject);
+            Debug.Log($"Target {gameObject.name} destroyed!");
         }
-
-        return closest;
-    }
-
-    private Animation animation;
-
-    private void Awake()
-    {
-       
-        if (targetList == null) targetList = new List<Target>();
-        targetList.Add(this);
-
-        
-        animation = transform.Find("Sprite").GetComponent<Animation>();
-        if (animation == null)
-        {
-            Debug.LogError("No Animation component found on 'Sprite' child object!");
-        }
-    }
-
-    private void OnDestroy()
-    {
-        
-        targetList.Remove(this);
-    }
-
-    public Vector3 GetPosition()
-    {
-        return transform.position;
-    }
-
-    public void Damage()
-    {
-  
-        animation.Play();
-
-        Vector3 randomOffset = new Vector3(0, 7.35f) + GetRandomDir() * UnityEngine.Random.Range(0f, 5f);
-
-        Debug.Log("Damage called with random offset: " + randomOffset);
-    }
-
-    public static Vector3 GetRandomDir()
-    {
-        float angle = UnityEngine.Random.Range(0f, 360f); 
-        return new Vector3(Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad), 0).normalized;
     }
 }
