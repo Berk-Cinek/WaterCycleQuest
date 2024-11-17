5e6fc41f4ec0f4cc7ae975727b329e69dade859d
Yağız Haşit <yagizhasit@hotmail.com>
2024-11-17 11:08:31 +0300

    Update PlayerAimWeapon.cs

--- Assets/Scripts/PlayerAimWeapon/PlayerAimWeapon.cs
+++ Assets/Scripts/PlayerAimWeapon/PlayerAimWeapon.cs
@@ -63,11 +63,27 @@ public class PlayerAimWeapon : MonoBehaviour
     {
         Vector3 mousePosition = GetMouseWorldPosition();
 
-        Vector3 aimDirection = (mousePosition - transform.position).normalized;
+        Vector3 aimDirection = (mousePosition - aimTransform.position).normalized;
 
         float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
 
         aimTransform.eulerAngles = new Vector3(0, 0, angle);
+
+        Vector3 aimLocalScale = Vector3.one;
+
+        if (angle > 90 || angle < -90)
+        {
+            aimLocalScale.y = -1f;
+        }
+
+        else
+        {
+            aimLocalScale.y = +1f;
+        }
+
+        aimTransform.localScale = aimLocalScale;
+
+
 
     }
 
