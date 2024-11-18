a4443e01f9eb70e7fc376bfb66aa8f8d1897a07f
Yağız Haşit <yagizhasit@hotmail.com>
2024-11-18 21:50:34 +0300

    DialogueSystem(InProgress)

---
+++ Assets/Scripts/Dialogue/DialogueObject.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Dialogue/DialogueObject")]
+public class DialogueObject : ScriptableObject
+
+{
+    [SerializeField][TextArea] private string[] dialogue;
+
+    public string[] Dialogue => dialogue;
+
+
+
+
+
+
+   
+}
--- Assets/Scripts/PlayerMovement/NewPlayerMovement.cs
+++ Assets/Scripts/PlayerMovement/NewPlayerMovement.cs
@@ -43,6 +43,7 @@ public class NewPlayerMovement : MonoBehaviour
 
     void Update()
     {
+        
         HandleMovement();
     }
 
